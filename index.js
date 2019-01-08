45bdd13f58fff8f11c616d5f59abaecacf16e53c
Merlin Beutlberger <m.beutlberger@sap.com>
2019-01-08 18:55:33 +0100

    [INTERNAL] JSDoc: Restructure modules via namespaces

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const ui5Project = {
+/**
+ * @module @ui5/project
+ * @public
+ */
+module.exports = {
+	/**
+	 * @public
+	 * @see @ui5/project.normalizer
+	 */
 	normalizer: require("./lib/normalizer"),
+	/**
+	 * @public
+	 * @see @ui5/project.projectPreprocessor
+	 */
 	projectPreprocessor: require("./lib/projectPreprocessor"),
 	translators: {
 		"npm": require("./lib/translators/npm"),
 		"static": require("./lib/translators/static")
 	}
 };
-
-module.exports = ui5Project;
