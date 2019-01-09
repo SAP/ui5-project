48b06e248ae68e704da9321b95ca16a289770439
Merlin Beutlberger <m.beutlberger@sap.com>
2019-01-09 15:44:31 +0100

    [INTERNAL] JSDoc: Refactor to use module: syntax and fix various docs

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 /**
- * @module @ui5/project
+ * @module module:@ui5/project
  * @public
  */
 module.exports = {
-	/**
-	 * @public
-	 * @see @ui5/project.normalizer
-	 */
 	normalizer: require("./lib/normalizer"),
-	/**
-	 * @public
-	 * @see @ui5/project.projectPreprocessor
-	 */
 	projectPreprocessor: require("./lib/projectPreprocessor"),
+	/**
+	 * @private
+	 * @see module:@ui5/project.translators
+	 * @namespace
+	 */
 	translators: {
 		"npm": require("./lib/translators/npm"),
 		"static": require("./lib/translators/static")
