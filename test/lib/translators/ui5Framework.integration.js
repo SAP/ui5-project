8d4d3d7c789fc5fa208479873ef587075858b24e
Matthias Osswald <mat.osswald@sap.com>
2020-02-18 17:07:51 +0100

    Add ui5framework OpenUI5 integration test

--- test/lib/translators/ui5Framework.integration.js
+++ test/lib/translators/ui5Framework.integration.js
@@ -54,7 +54,7 @@ test.serial("ui5Framework translator should not be called when no framework conf
 	ui5FrameworkMock.verify();
 });
 
-test.serial("ui5Framework translator should enhance tree with UI5 framework libraries", async (t) => {
+test.serial("SAPUI5: ui5Framework translator should enhance tree with UI5 framework libraries", async (t) => {
 	const translatorTree = {
 		id: "test-id",
 		version: "1.2.3",
@@ -89,6 +89,7 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 				name: "test-project"
 			},
 			framework: {
+				name: "SAPUI5",
 				version: "1.75.0",
 				libraries: [
 					{
@@ -128,7 +129,7 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 				name: "test-project-no-framework"
 			}
 		}])
-		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib1", "1.75.1", "ui5.yaml"))
+		.withArgs(path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib1", "1.75.1", "ui5.yaml"))
 		.resolves([{
 			specVersion: "1.0",
 			type: "library",
@@ -137,7 +138,7 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 			},
 			framework: {libraries: []}
 		}])
-		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib2", "1.75.2", "ui5.yaml"))
+		.withArgs(path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib2", "1.75.2", "ui5.yaml"))
 		.resolves([{
 			specVersion: "1.0",
 			type: "library",
@@ -146,7 +147,7 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 			},
 			framework: {libraries: []}
 		}])
-		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib3", "1.75.3", "ui5.yaml"))
+		.withArgs(path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib3", "1.75.3", "ui5.yaml"))
 		.resolves([{
 			specVersion: "1.0",
 			type: "library",
@@ -183,13 +184,13 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 		"metadata.json"), {
 		libraries: {
 			"sap.ui.lib1": {
-				npmPackageName: "@openui5/sap.ui.lib1",
+				npmPackageName: "@sapui5/sap.ui.lib1",
 				version: "1.75.1",
 				dependencies: [],
 				optionalDependencies: []
 			},
 			"sap.ui.lib2": {
-				npmPackageName: "@openui5/sap.ui.lib2",
+				npmPackageName: "@sapui5/sap.ui.lib2",
 				version: "1.75.2",
 				dependencies: [
 					"sap.ui.lib3"
@@ -197,7 +198,336 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 				optionalDependencies: []
 			},
 			"sap.ui.lib3": {
-				npmPackageName: "@openui5/sap.ui.lib3",
+				npmPackageName: "@sapui5/sap.ui.lib3",
+				version: "1.75.3",
+				dependencies: [],
+				optionalDependencies: [
+					"sap.ui.lib4"
+				]
+			},
+			"sap.ui.lib4": {
+				npmPackageName: "@openui5/sap.ui.lib4",
+				version: "1.75.4",
+				dependencies: [],
+				optionalDependencies: []
+			}
+		}
+	});
+
+	const expectedTree = {
+		_level: 0,
+		id: "test-id",
+		kind: "project",
+		version: "1.2.3",
+		path: path.join(fakeBaseDir, "application-project"),
+		specVersion: "1.1",
+		type: "application",
+		metadata: {
+			name: "test-project"
+		},
+		framework: {
+			name: "SAPUI5",
+			version: "1.75.0",
+			libraries: [
+				{
+					name: "sap.ui.lib1"
+				},
+				{
+					name: "sap.ui.lib4",
+					optional: true
+				}
+			]
+		},
+		dependencies: [
+			{
+				_level: 1,
+				id: "test-dependency-id",
+				version: "4.5.6",
+				path: path.join(fakeBaseDir, "dependency-project"),
+				specVersion: "1.1",
+				kind: "project",
+				type: "library",
+				metadata: {
+					name: "test-project"
+				},
+				framework: {
+					version: "1.99.0",
+					libraries: [
+						{
+							name: "sap.ui.lib1"
+						},
+						{
+							name: "sap.ui.lib2"
+						}
+					]
+				},
+				dependencies: [
+					{
+						_level: 1,
+						id: "@sapui5/sap.ui.lib1",
+						version: "1.75.1",
+						path: path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib1", "1.75.1"),
+						specVersion: "1.0",
+						kind: "project",
+						type: "library",
+						metadata: {
+							name: "sap.ui.lib1"
+						},
+						framework: {
+							libraries: []
+						},
+						dependencies: []
+					},
+					{
+						_level: 1,
+						id: "@sapui5/sap.ui.lib2",
+						version: "1.75.2",
+						path: path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib2", "1.75.2"),
+						specVersion: "1.0",
+						kind: "project",
+						type: "library",
+						metadata: {
+							name: "sap.ui.lib2"
+						},
+						framework: {
+							libraries: []
+						},
+						dependencies: [
+							{
+								_level: 2,
+								id: "@sapui5/sap.ui.lib3",
+								version: "1.75.3",
+								path: path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib3", "1.75.3"),
+								specVersion: "1.0",
+								kind: "project",
+								type: "library",
+								metadata: {
+									name: "sap.ui.lib3"
+								},
+								framework: {
+									libraries: []
+								},
+								dependencies: [
+									{
+										_level: 1,
+										id: "@openui5/sap.ui.lib4",
+										version: "1.75.4",
+										path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.4"),
+										specVersion: "1.0",
+										kind: "project",
+										type: "library",
+										metadata: {
+											name: "sap.ui.lib4"
+										},
+										framework: {
+											libraries: []
+										},
+										dependencies: []
+									},
+								]
+							}
+						]
+					}
+				]
+			},
+			{
+				_level: 1,
+				id: "test-dependency-no-framework-id",
+				version: "7.8.9",
+				path: path.join(fakeBaseDir, "dependency-project-no-framework"),
+				specVersion: "1.1",
+				kind: "project",
+				type: "library",
+				metadata: {
+					name: "test-project-no-framework"
+				},
+				dependencies: []
+			},
+			{
+				_level: 1,
+				id: "@sapui5/sap.ui.lib1",
+				version: "1.75.1",
+				path: path.join(ui5PackagesBaseDir, "@sapui5", "sap.ui.lib1", "1.75.1"),
+				specVersion: "1.0",
+				kind: "project",
+				type: "library",
+				metadata: {
+					name: "sap.ui.lib1"
+				},
+				framework: {
+					libraries: []
+				},
+				dependencies: []
+			},
+			{
+				_level: 1,
+				id: "@openui5/sap.ui.lib4",
+				version: "1.75.4",
+				path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.4"),
+				specVersion: "1.0",
+				kind: "project",
+				type: "library",
+				metadata: {
+					name: "sap.ui.lib4"
+				},
+				framework: {
+					libraries: []
+				},
+				dependencies: []
+			},
+		]
+	};
+
+	const tree = await normalizer.generateProjectTree();
+
+	t.deepEqual(tree, expectedTree, "Returned tree should be correct");
+});
+
+test.serial.skip("OpenUI5: ui5Framework translator should enhance tree with UI5 framework libraries", async (t) => {
+	const translatorTree = {
+		id: "test-id",
+		version: "1.2.3",
+		path: path.join(fakeBaseDir, "application-project"),
+		dependencies: [
+			{
+				id: "test-dependency-id",
+				version: "4.5.6",
+				path: path.join(fakeBaseDir, "dependency-project"),
+				dependencies: []
+			},
+			{
+				id: "test-dependency-no-framework-id",
+				version: "7.8.9",
+				path: path.join(fakeBaseDir, "dependency-project-no-framework"),
+				dependencies: []
+			}
+		]
+	};
+
+	sinon.stub(normalizer, "generateDependencyTree").resolves(translatorTree);
+
+	sinon.stub(projectPreprocessor.ProjectPreprocessor.prototype, "readConfigFile")
+		.callsFake(async (configPath) => {
+			throw new Error("ProjectPreprocessor#readConfigFile stub called with unknown configPath: " + configPath);
+		})
+		.withArgs(path.join(fakeBaseDir, "application-project", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.1",
+			type: "application",
+			metadata: {
+				name: "test-project"
+			},
+			framework: {
+				name: "OpenUI5",
+				version: "1.75.0",
+				libraries: [
+					{
+						name: "sap.ui.lib1"
+					},
+					{
+						name: "sap.ui.lib4",
+						optional: true
+					}
+				]
+			}
+		}])
+		.withArgs(path.join(fakeBaseDir, "dependency-project", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.1",
+			type: "library",
+			metadata: {
+				name: "test-project"
+			},
+			framework: {
+				version: "1.99.0",
+				libraries: [
+					{
+						name: "sap.ui.lib1"
+					},
+					{
+						name: "sap.ui.lib2"
+					}
+				]
+			}
+		}])
+		.withArgs(path.join(fakeBaseDir, "dependency-project-no-framework", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.1",
+			type: "library",
+			metadata: {
+				name: "test-project-no-framework"
+			}
+		}])
+		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib1", "1.75.0", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.0",
+			type: "library",
+			metadata: {
+				name: "sap.ui.lib1"
+			},
+			framework: {libraries: []}
+		}])
+		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib2", "1.75.0", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.0",
+			type: "library",
+			metadata: {
+				name: "sap.ui.lib2"
+			},
+			framework: {libraries: []}
+		}])
+		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib3", "1.75.0", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.0",
+			type: "library",
+			metadata: {
+				name: "sap.ui.lib3"
+			},
+			framework: {libraries: []}
+		}])
+		.withArgs(path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.0", "ui5.yaml"))
+		.resolves([{
+			specVersion: "1.0",
+			type: "library",
+			metadata: {
+				name: "sap.ui.lib4"
+			},
+			framework: {libraries: []}
+		}]);
+
+	// Prevent applying types as this would require a lot of mocking
+	sinon.stub(projectPreprocessor.ProjectPreprocessor.prototype, "applyType");
+
+	sinon.stub(libnpmconfig, "read").returns({
+		toJSON: sinon.stub().returns({
+			registry: "https://registry.fake",
+			cache: path.join(ui5FrameworkBaseDir, "cacache"),
+			proxy: ""
+		})
+	});
+	sinon.stub(pacote, "extract").resolves();
+
+	mock(path.join(fakeBaseDir,
+		"homedir", ".ui5", "framework", "packages",
+		"@sapui5", "distribution-metadata", "1.75.0",
+		"metadata.json"), {
+		libraries: {
+			"sap.ui.lib1": {
+				npmPackageName: "@sapui5/sap.ui.lib1",
+				version: "1.75.1",
+				dependencies: [],
+				optionalDependencies: []
+			},
+			"sap.ui.lib2": {
+				npmPackageName: "@sapui5/sap.ui.lib2",
+				version: "1.75.2",
+				dependencies: [
+					"sap.ui.lib3"
+				],
+				optionalDependencies: []
+			},
+			"sap.ui.lib3": {
+				npmPackageName: "@sapui5/sap.ui.lib3",
 				version: "1.75.3",
 				dependencies: [],
 				optionalDependencies: [
@@ -263,8 +593,8 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 					{
 						_level: 1,
 						id: "@openui5/sap.ui.lib1",
-						version: "1.75.1",
-						path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib1", "1.75.1"),
+						version: "1.75.0",
+						path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib1", "1.75.0"),
 						specVersion: "1.0",
 						kind: "project",
 						type: "library",
@@ -279,8 +609,8 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 					{
 						_level: 1,
 						id: "@openui5/sap.ui.lib2",
-						version: "1.75.2",
-						path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib2", "1.75.2"),
+						version: "1.75.0",
+						path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib2", "1.75.0"),
 						specVersion: "1.0",
 						kind: "project",
 						type: "library",
@@ -294,8 +624,8 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 							{
 								_level: 2,
 								id: "@openui5/sap.ui.lib3",
-								version: "1.75.3",
-								path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib3", "1.75.3"),
+								version: "1.75.0",
+								path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib3", "1.75.0"),
 								specVersion: "1.0",
 								kind: "project",
 								type: "library",
@@ -309,8 +639,8 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 									{
 										_level: 1,
 										id: "@openui5/sap.ui.lib4",
-										version: "1.75.4",
-										path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.4"),
+										version: "1.75.0",
+										path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.0"),
 										specVersion: "1.0",
 										kind: "project",
 										type: "library",
@@ -344,8 +674,8 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 			{
 				_level: 1,
 				id: "@openui5/sap.ui.lib1",
-				version: "1.75.1",
-				path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib1", "1.75.1"),
+				version: "1.75.0",
+				path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib1", "1.75.0"),
 				specVersion: "1.0",
 				kind: "project",
 				type: "library",
@@ -360,8 +690,8 @@ test.serial("ui5Framework translator should enhance tree with UI5 framework libr
 			{
 				_level: 1,
 				id: "@openui5/sap.ui.lib4",
-				version: "1.75.4",
-				path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.4"),
+				version: "1.75.0",
+				path: path.join(ui5PackagesBaseDir, "@openui5", "sap.ui.lib4", "1.75.0"),
 				specVersion: "1.0",
 				kind: "project",
 				type: "library",
