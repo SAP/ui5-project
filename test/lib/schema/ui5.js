234ad0a8747143f0e3646254ef60d12de43ac1c6
Matthias Osswald <mat.osswald@sap.com>
2020-03-13 08:40:35 +0100

    Fix schema, add more tests + coverage

--- test/lib/schema/ui5.js
+++ test/lib/schema/ui5.js
@@ -1,18 +1,33 @@
 const test = require("ava");
-
-const validate = require("../../../lib/schema/validate");
+const Ajv = require("Ajv");
+const ajvCoverage = require("./_ajvCoverage");
+const {_Validator: Validator} = require("../../../lib/schema/validate");
 
 function assertValidation(t, data, expectedErrors = undefined) {
-	const errors = validate(data);
+	const errors = validator.validate(data);
 	if (expectedErrors) {
 		t.deepEqual(errors, expectedErrors);
-		t.is(errors.length, expectedErrors.length);
 	} else {
 		// Use JSON string for better logging in case of errors
 		t.is(JSON.stringify(errors, null, 2), undefined);
 		t.is(errors, undefined);
 	}
 }
+
+let validator;
+let createReport;
+
+test.before(() => {
+	const {ajv, createReport: _createReport} = ajvCoverage(Ajv, {
+		allErrors: true
+	});
+	createReport = _createReport;
+	validator = new Validator(ajv);
+});
+
+test.after.always(() => {
+	createReport(global.__coverage__);
+});
 
 test("Missing specVersion, type, metadata", (t) => {
 	assertValidation(t, {}, [
@@ -44,6 +59,163 @@ test("Missing specVersion, type, metadata", (t) => {
 			schemaPath: "#/required"
 		}
 
+	]);
+});
+
+test("Missing type, metadata", (t) => {
+	assertValidation(t, {
+		"specVersion": "2.0"
+	}, [
+		{
+			dataPath: "",
+			keyword: "required",
+			message: "should have required property 'metadata'",
+			params: {
+				missingProperty: "metadata",
+			},
+			schemaPath: "#/required"
+		},
+		{
+			dataPath: "",
+			keyword: "required",
+			message: "should have required property 'type'",
+			params: {
+				missingProperty: "type",
+			},
+			schemaPath: "#/required"
+		}
+	]);
+});
+
+test("Invalid specVersion", (t) => {
+	assertValidation(t, {
+		"specVersion": "0.0"
+	}, [
+		{
+			dataPath: ".specVersion",
+			keyword: "enum",
+			message: "should be equal to one of the allowed values",
+			params: {
+				allowedValues: [
+					"2.0",
+					"1.1",
+					"1.0",
+					"0.1",
+				],
+			},
+			schemaPath: "#/properties/specVersion/enum"
+		}
+	]);
+});
+
+test("Invalid type", (t) => {
+	assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "foo",
+		"metadata": {
+			"name": "foo"
+		}
+	}, [
+		{
+			dataPath: ".type",
+			keyword: "enum",
+			message: "should be equal to one of the allowed values",
+			params: {
+				allowedValues: [
+					"application",
+					"library",
+					"theme-library",
+					"module"
+				]
+			},
+			schemaPath: "#/properties/type/enum"
+		}
+	]);
+});
+
+test("Invalid kind", (t) => {
+	assertValidation(t, {
+		"specVersion": "2.0",
+		"kind": "foo",
+		"metadata": {
+			"name": "foo"
+		}
+	}, [
+		{
+			dataPath: ".kind",
+			keyword: "enum",
+			message: "should be equal to one of the allowed values",
+			params: {
+				allowedValues: [
+					"project",
+					"extension",
+					null
+				],
+			},
+			schemaPath: "#/properties/kind/enum"
+		}
+	]);
+});
+
+test("Invalid metadata.name", (t) => {
+	assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "application",
+		"metadata": {
+			"name": {}
+		}
+	}, [
+		{
+			dataPath: ".metadata.name",
+			keyword: "type",
+			message: "should be string",
+			params: {
+				type: "string"
+			},
+			schemaPath: "../ui5.json#/definitions/metadata/properties/name/type"
+		}
+	]);
+});
+
+test("Invalid metadata.copyright", (t) => {
+	assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "application",
+		"metadata": {
+			"name": "foo",
+			"copyright": 123
+		}
+	}, [
+		{
+			dataPath: ".metadata.copyright",
+			keyword: "type",
+			message: "should be string",
+			params: {
+				type: "string"
+			},
+			schemaPath: "../ui5.json#/definitions/metadata/properties/copyright/type"
+		}
+	]);
+});
+
+test("Additional metadata property", (t) => {
+	assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "application",
+		"metadata": {
+			"name": "foo",
+			"copyrihgt": "typo"
+		}
+	}, [
+		{
+			dataPath: ".metadata",
+			keyword: "additionalProperties",
+			message: "should NOT have additional properties",
+			params: {
+				additionalProperty: "copyrihgt"
+			},
+			schemaPath: "../ui5.json#/definitions/metadata/additionalProperties"
+		}
 	]);
 });
 
@@ -109,23 +281,100 @@ test("type: application", (t) => {
 			}
 		}
 	});
+});
+
+test("type: application (invalid resources configuration)", (t) => {
 	assertValidation(t, {
 		"specVersion": "2.0",
 		"type": "application",
 		"metadata": {
-			"name": "com.sap.ui5.test",
-			"copyright": "yes"
+			"name": "com.sap.ui5.test"
 		},
-		"foo": true
-	}, [{
-		"keyword": "additionalProperties",
-		"dataPath": "",
-		"schemaPath": "#/additionalProperties",
-		"params": {
-			"additionalProperty": "foo"
+		"resources": {
+			"configuration": {
+				"propertiesFileSourceEncoding": "FOO",
+				"paths": {
+					"app": "webapp",
+					"webapp": {
+						"path": "invalid"
+					}
+				},
+				"notAllowed": true
+			},
+			"notAllowed": true
+		}
+	}, [
+		{
+			dataPath: ".resources",
+			keyword: "additionalProperties",
+			message: "should NOT have additional properties",
+			params: {
+				additionalProperty: "notAllowed",
+			},
+			schemaPath: "#/properties/resources/additionalProperties",
 		},
-		"message": "should NOT have additional properties"
-	}]);
+		{
+			dataPath: ".resources.configuration",
+			keyword: "additionalProperties",
+			message: "should NOT have additional properties",
+			params: {
+				additionalProperty: "notAllowed",
+			},
+			schemaPath: "#/properties/resources/properties/configuration/additionalProperties",
+		},
+		{
+			dataPath: ".resources.configuration.propertiesFileSourceEncoding",
+			keyword: "enum",
+			message: "should be equal to one of the allowed values",
+			params: {
+				allowedValues: [
+					"UTF-8",
+					"ISO-8859-1"
+				],
+			},
+			schemaPath: "#/definitions/resources-configuration-propertiesFileSourceEncoding/enum"
+		},
+		{
+			dataPath: ".resources.configuration.paths",
+			keyword: "additionalProperties",
+			message: "should NOT have additional properties",
+			params: {
+				additionalProperty: "app",
+			},
+			schemaPath: "#/properties/resources/properties/configuration/properties/paths/additionalProperties",
+		},
+		{
+			dataPath: ".resources.configuration.paths.webapp",
+			keyword: "type",
+			message: "should be string",
+			params: {
+				type: "string"
+			},
+			schemaPath: "#/properties/resources/properties/configuration/properties/paths/properties/webapp/type"
+		}
+	]);
+	assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "application",
+		"metadata": {
+			"name": "com.sap.ui5.test"
+		},
+		"resources": {
+			"configuration": {
+				"paths": "webapp"
+			}
+		}
+	}, [
+		{
+			dataPath: ".resources.configuration.paths",
+			keyword: "type",
+			message: "should be object",
+			params: {
+				type: "object"
+			},
+			schemaPath: "#/properties/resources/properties/configuration/properties/paths/type"
+		}
+	]);
 });
 
 test("type: library", (t) => {
