16894e11c5c21a77a405431dfaf5d8642accfc1d
Matthias Osswald <mat.osswald@sap.com>
2020-04-02 18:35:59 +0200

    [FIX] Schema: Add missing metadata properties
    Also refactored the schema to only test and validate the metadata where
    required.

--- test/lib/validation/schema/specVersion/2.0/kind/project/module.js
+++ test/lib/validation/schema/specVersion/2.0/kind/project/module.js
@@ -122,3 +122,241 @@ test("No builder, server configuration", async (t) => {
 		schemaPath: "#/additionalProperties"
 	}]);
 });
+
+test("No metadata", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module"
+	}, [{
+		dataPath: "",
+		keyword: "required",
+		message: "should have required property 'metadata'",
+		params: {
+			missingProperty: "metadata",
+		},
+		schemaPath: "#/required",
+	}]);
+});
+
+test("Metadata not type object", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": "foo"
+	}, [{
+		dataPath: "/metadata",
+		keyword: "type",
+		message: "should be object",
+		params: {
+			type: "object",
+		},
+		schemaPath: "../project.json#/definitions/metadata/type",
+	}]);
+});
+
+test("No metadata.name", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {}
+	}, [{
+		dataPath: "/metadata",
+		keyword: "required",
+		message: "should have required property 'name'",
+		params: {
+			missingProperty: "name",
+		},
+		schemaPath: "../project.json#/definitions/metadata/required",
+	}]);
+});
+
+test("Invalid metadata.name", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "library",
+		"metadata": {
+			"name": {}
+		}
+	}, [
+		{
+			dataPath: "/metadata/name",
+			keyword: "type",
+			message: "should be string",
+			params: {
+				type: "string"
+			},
+			schemaPath: "../project.json#/definitions/metadata/properties/name/type",
+		}
+	]);
+});
+
+test("Invalid metadata.copyright", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "library",
+		"metadata": {
+			"name": "foo",
+			"copyright": 123
+		}
+	}, [
+		{
+			dataPath: "/metadata/copyright",
+			keyword: "type",
+			message: "should be string",
+			params: {
+				type: "string"
+			},
+			schemaPath: "../project.json#/definitions/metadata/properties/copyright/type",
+		}
+	]);
+});
+
+test("Additional metadata property", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "library",
+		"metadata": {
+			"name": "foo",
+			"copyrihgt": "typo"
+		}
+	}, [
+		{
+			dataPath: "/metadata",
+			keyword: "additionalProperties",
+			message: "should NOT have additional properties",
+			params: {
+				additionalProperty: "copyrihgt"
+			},
+			schemaPath: "../project.json#/definitions/metadata/additionalProperties",
+		}
+	]);
+});
+
+test("metadata.deprecated: true", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"deprecated": true
+		}
+	});
+});
+
+test("metadata.deprecated: false", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"deprecated": false
+		}
+	});
+});
+
+test("Invalid metadata.deprecated", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"deprecated": "Yes"
+		}
+	}, [
+		{
+			dataPath: "/metadata/deprecated",
+			keyword: "type",
+			message: "should be boolean",
+			params: {
+				type: "boolean",
+			},
+			schemaPath: "../project.json#/definitions/metadata/properties/deprecated/type",
+		}
+	]);
+});
+
+test("metadata.sapInternal: true", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"sapInternal": true
+		}
+	});
+});
+
+test("metadata.sapInternal: false", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"sapInternal": false
+		}
+	});
+});
+
+test("Invalid metadata.sapInternal", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"sapInternal": "Yes"
+		}
+	}, [
+		{
+			dataPath: "/metadata/sapInternal",
+			keyword: "type",
+			message: "should be boolean",
+			params: {
+				type: "boolean",
+			},
+			schemaPath: "../project.json#/definitions/metadata/properties/sapInternal/type",
+		}
+	]);
+});
+
+test("metadata.allowSapInternal: true", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"allowSapInternal": true
+		}
+	});
+});
+
+test("metadata.allowSapInternal: false", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"allowSapInternal": false
+		}
+	});
+});
+
+test("Invalid metadata.allowSapInternal", async (t) => {
+	await assertValidation(t, {
+		"specVersion": "2.0",
+		"type": "module",
+		"metadata": {
+			"name": "my-module",
+			"allowSapInternal": "Yes"
+		}
+	}, [
+		{
+			dataPath: "/metadata/allowSapInternal",
+			keyword: "type",
+			message: "should be boolean",
+			params: {
+				type: "boolean",
+			},
+			schemaPath: "../project.json#/definitions/metadata/properties/allowSapInternal/type",
+		}
+	]);
+});
