db2d45af62b6edbaeef4c882e665a9ba6ac03eb1
Merlin Beutlberger <m.beutlberger@sap.com>
2021-02-18 17:37:10 +0100

    Merge Configuration and Specification into one entity
    Implement full Application spec

--- test/lib/specifications/Project.js
+++ test/lib/specifications/Project.js
@@ -5,32 +5,36 @@ const Configuration = require("../../../lib/specifications/Configuration");
 
 const applicationAPath = path.join(__dirname, "..", "..", "fixtures", "application.a");
 
-const basicConfiguration = new Configuration({
-	specVersion: "2.3",
-	kind: "project",
-	metadata: {name: "application.a"}
+test.beforeEach(async (t) => {
+	t.context.basicConfiguration = new Configuration({
+		specVersion: "2.3",
+		kind: "project",
+		type: "application",
+		metadata: {name: "application.a"}
+	});
+
+	t.context.basicProjectInput = {
+		id: "application.a.id",
+		version: "1.0.0",
+		modulePath: applicationAPath,
+		configuration: t.context.basicConfiguration
+	};
 });
-const basicProjectInput = {
-	id: "application.a.id",
-	version: "1.0.0",
-	modulePath: applicationAPath,
-	configuration: basicConfiguration
-};
 
 test("Instantiate a basic project", async (t) => {
-	const project = new Project(basicProjectInput);
+	const project = new Project(t.context.basicProjectInput);
 	t.is(project.getName(), "application.a", "Returned correct name");
 	t.is(project.getVersion(), "1.0.0", "Returned correct version");
 	t.is(project.getPath(), applicationAPath, "Returned correct project path");
 });
 
 test("_getConfiguration", async (t) => {
-	const project = new Project(basicProjectInput);
-	t.is(await project._getConfiguration(), basicConfiguration, "Returned correct configuration instance");
+	const project = new Project(t.context.basicProjectInput);
+	t.is(await project._getConfiguration(), t.context.basicConfiguration, "Returned correct configuration instance");
 });
 
 test("Access project root resources via reader", async (t) => {
-	const project = new Project(basicProjectInput);
+	const project = new Project(t.context.basicProjectInput);
 	const rootReader = await project.getRootReader();
 	const packageJsonResource = await rootReader.byPath("/package.json");
 	t.is(packageJsonResource.getPath(), "/package.json", "Successfully retrieved root resource");
