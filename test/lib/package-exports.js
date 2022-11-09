9306f8c40f8bea50ba6859f7b54672dbb65abc66
Merlin Beutlberger <m.beutlberger@sap.com>
2022-11-09 20:43:41 +0100

    [INTERNAL] Sapui5MavenSnapshotResolver: Implement fetchAllVersions

--- test/lib/package-exports.js
+++ test/lib/package-exports.js
@@ -13,13 +13,14 @@ test("export of package.json", (t) => {
 // Check number of definied exports
 test("check number of exports", (t) => {
 	const packageJson = require("@ui5/project/package.json");
-	t.is(Object.keys(packageJson.exports).length, 8);
+	t.is(Object.keys(packageJson.exports).length, 9);
 });
 
 // Public API contract (exported modules)
 [
 	"ui5Framework/Openui5Resolver",
 	"ui5Framework/Sapui5Resolver",
+	"ui5Framework/Sapui5MavenSnapshotResolver",
 	"validation/validator",
 	"validation/ValidationError",
 	"graph/ProjectGraph",
