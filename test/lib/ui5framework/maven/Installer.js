2904c418d6904dce2f30d123a4097273112ebcea
Yavor Ivanov <yavor.ivanov@sap.com>
2023-02-07 09:46:20 +0200

    Fix merge conflicts

--- test/lib/ui5framework/maven/Installer.js
+++ test/lib/ui5framework/maven/Installer.js
@@ -2,9 +2,6 @@ import test from "ava";
 import sinon from "sinon";
 import esmock from "esmock";
 import path from "node:path";
-import {fileURLToPath} from "node:url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 test.beforeEach(async (t) => {
 	t.context.mkdirpStub = sinon.stub().resolves();
