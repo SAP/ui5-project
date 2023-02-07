4cd1caf3733372b80792d3b57d22164110ad51de
Yavor Ivanov <yavor.ivanov@sap.com>
2023-02-07 16:16:56 +0200

    Fix mkdrip stubbing

--- test/lib/ui5framework/npm/Installer.js
+++ test/lib/ui5framework/npm/Installer.js
@@ -7,6 +7,7 @@ import {fileURLToPath} from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 test.beforeEach(async (t) => {
+	t.context.mkdirpStub = sinon.stub().resolves();
 	t.context.rimrafStub = sinon.stub().resolves();
 
 	t.context.lockStub = sinon.stub();
