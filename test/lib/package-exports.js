11ff2a59b917e0b426e83b387813b1613ef1b680
Yavor Ivanov <yavor.ivanov@sap.com>
2023-02-07 15:55:00 +0200

    Fix tests without default logger export

--- test/lib/package-exports.js
+++ test/lib/package-exports.js
@@ -13,7 +13,7 @@ test("export of package.json", (t) => {
 // Check number of definied exports
 test("check number of exports", (t) => {
 	const packageJson = require("@ui5/project/package.json");
-	t.is(Object.keys(packageJson.exports).length, 10);
+	t.is(Object.keys(packageJson.exports).length, 11);
 });
 
 // Public API contract (exported modules)
