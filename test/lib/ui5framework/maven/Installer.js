edceb3c5772eb2c89e91b203bdfe76274fa6cd3b
Yavor Ivanov <yavor.ivanov@sap.com>
2023-02-10 13:22:17 +0200

    Add more tests for maven/Install

--- test/lib/ui5framework/maven/Installer.js
+++ test/lib/ui5framework/maven/Installer.js
@@ -2,17 +2,31 @@ import test from "ava";
 import sinon from "sinon";
 import esmock from "esmock";
 import path from "node:path";
+import fs from "graceful-fs";
 
 test.beforeEach(async (t) => {
 	t.context.mkdirpStub = sinon.stub().resolves();
 	t.context.rimrafStub = sinon.stub().yieldsAsync();
+	t.context.readFileStub = sinon.stub();
+	t.context.writeFileStub = sinon.stub();
+	t.context.renameStub = sinon.stub().returns();
+
+	t.context.promisifyStub = sinon.stub();
+	t.context.promisifyStub.withArgs(fs.readFile).callsFake(() => t.context.readFileStub);
+	t.context.promisifyStub.withArgs(fs.readFile).callsFake(() => t.context.readFileStub);
+	t.context.promisifyStub.withArgs(fs.rename).callsFake(() => t.context.renameStub);
 
 	t.context.lockStub = sinon.stub();
 	t.context.unlockStub = sinon.stub();
-	t.context.renameStub = sinon.stub().yieldsAsync();
-	t.context.statStub = sinon.stub().yieldsAsync();
 
-	t.context.AbstractResolver = await esmock.p("../../../../lib/ui5Framework/AbstractInstaller.js", {
+	class Zzz {
+		extract = sinon.stub().resolves();
+		close = sinon.stub().resolves();
+	}
+
+	t.context.zipStub = Zzz;
+
+	t.context.AbstractInstaller = await esmock.p("../../../../lib/ui5Framework/AbstractInstaller.js", {
 		"mkdirp": t.context.mkdirpStub,
 		"rimraf": t.context.rimrafStub,
 		"lockfile": {
@@ -22,18 +36,21 @@ test.beforeEach(async (t) => {
 	});
 
 	t.context.Installer = await esmock.p("../../../../lib/ui5Framework/maven/Installer.js", {
-		"../../../../lib/ui5Framework/AbstractInstaller.js": t.context.AbstractResolver,
+		"../../../../lib/ui5Framework/AbstractInstaller.js": t.context.AbstractInstaller,
 		"mkdirp": t.context.mkdirpStub,
 		"rimraf": t.context.rimrafStub,
-		"graceful-fs": {
-			rename: t.context.renameStub,
-			stat: t.context.statStub
+		"node:util": {
+			"promisify": t.context.promisifyStub,
+		},
+		"node-stream-zip": {
+			"async": t.context.zipStub
 		}
 	});
 });
 
 test.afterEach.always((t) => {
 	sinon.restore();
+	esmock.purge(t.context.AbstractInstaller);
 	esmock.purge(t.context.Installer);
 });
 
@@ -125,3 +142,93 @@ test("Installer: _getLockPath", (t) => {
 
 	t.is(lockPath, path.join("/ui5Home/", "framework", "locks", "package-@openui5-sap.ui.lib1@1.2.3-SNAPSHOT.lock"));
 });
+
+test("Installer: readJson", async (t) => {
+	const jsonStub = {json: "response"};
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	t.context.readFileStub.resolves(JSON.stringify(jsonStub));
+
+	const jsonResponse = await installer.readJson("package-@openui5/sap.ui.lib1@1.2.3-SNAPSHOT");
+
+	t.deepEqual(jsonResponse, jsonStub);
+});
+
+test("Installer: installPackage", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	sinon.stub(installer, "_fetchArtifactMetadata").resolves({revision: "1.22"});
+	sinon.stub(installer, "_pathExists").resolves(false);
+	sinon.stub(installer, "_synchronize").callsFake( async (pckg, callback) => await callback());
+	sinon.stub(installer, "installArtifact").resolves({
+		artifactPath: "/ui5Home/framework/artifacts/com_sap_ui5_dist-sapui5-sdk-dist/1.22/npm-sources.zip",
+		removeArtifact: sinon.stub().resolves()
+	});
+
+	const installedPackage = await installer.installPackage({
+		pkgName: "@sapui5/distribution-metadata",
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		classifier: "npm-sources",
+		extension: "jar",
+	});
+
+	t.deepEqual(
+		installedPackage,
+		{pkgPath:
+				"/ui5Home/framework/packages/@sapui5/distribution-metadata/1.22"},
+		"Install the correct package"
+	);
+});
+
+test("Installer: installArtifact", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	sinon.stub(installer, "_fetchArtifactMetadata").resolves({revision: "1.22"});
+	sinon.stub(installer, "_pathExists").resolves(false);
+	sinon.stub(installer, "_synchronize").callsFake( async (pckg, callback) => await callback());
+	sinon.stub(installer, "getRegistry").callsFake(() => {
+		return {
+			requestArtifact: sinon.stub().resolves()
+		};
+	});
+
+	const installedArtifact = await installer.installArtifact({
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		extension: "jar",
+	});
+
+	t.is(
+		installedArtifact.artifactPath,
+		"/ui5Home/framework/artifacts/com_sap_ui5_dist-sapui5-sdk-dist/1.22.jar",
+		"artifactPath correctly resolved"
+	);
+
+	t.is(
+		typeof installedArtifact.removeArtifact,
+		"function",
+		"removeArtifact method"
+	);
+});
+
