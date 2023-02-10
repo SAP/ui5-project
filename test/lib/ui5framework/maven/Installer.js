441b8805ab3fc673853650712b686eeeabd33ebb
Yavor Ivanov <yavor.ivanov@sap.com>
2023-02-10 14:31:25 +0200

    Add more tests to maven/Installer

--- test/lib/ui5framework/maven/Installer.js
+++ test/lib/ui5framework/maven/Installer.js
@@ -18,13 +18,10 @@ test.beforeEach(async (t) => {
 
 	t.context.lockStub = sinon.stub();
 	t.context.unlockStub = sinon.stub();
-
-	class Zzz {
+	t.context.zipStub = class StreamZipStub {
 		extract = sinon.stub().resolves();
 		close = sinon.stub().resolves();
-	}
-
-	t.context.zipStub = Zzz;
+	};
 
 	t.context.AbstractInstaller = await esmock.p("../../../../lib/ui5Framework/AbstractInstaller.js", {
 		"mkdirp": t.context.mkdirpStub,
@@ -129,7 +126,28 @@ test.serial("Installer: fetchPackageVersions", async (t) => {
 		"requestMavenMetadata was called with correct arguments");
 });
 
-test("Installer: _getLockPath", (t) => {
+test.serial("Installer: fetchPackageVersions throws", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	const registry = installer.getRegistry();
+	sinon.stub(registry, "requestMavenMetadata").resolves({});
+
+	await t.throwsAsync(
+		installer.fetchPackageVersions({
+			groupId: "ui5.corp",
+			artifactId: "great-thing",
+		}),
+		{message: "Missing Maven metadata for artifact ui5.corp:great-thing"}
+	);
+});
+
+test.serial("Installer: _getLockPath", (t) => {
 	const {Installer} = t.context;
 
 	const installer = new Installer({
@@ -143,7 +161,7 @@ test("Installer: _getLockPath", (t) => {
 	t.is(lockPath, path.join("/ui5Home/", "framework", "locks", "package-@openui5-sap.ui.lib1@1.2.3-SNAPSHOT.lock"));
 });
 
-test("Installer: readJson", async (t) => {
+test.serial("Installer: readJson", async (t) => {
 	const jsonStub = {json: "response"};
 	const {Installer} = t.context;
 
@@ -160,7 +178,7 @@ test("Installer: readJson", async (t) => {
 	t.deepEqual(jsonResponse, jsonStub);
 });
 
-test("Installer: installPackage", async (t) => {
+test.serial("Installer: installPackage", async (t) => {
 	const {Installer} = t.context;
 
 	const installer = new Installer({
@@ -194,7 +212,7 @@ test("Installer: installPackage", async (t) => {
 	);
 });
 
-test("Installer: installArtifact", async (t) => {
+test.serial("Installer: installArtifact", async (t) => {
 	const {Installer} = t.context;
 
 	const installer = new Installer({
@@ -232,3 +250,137 @@ test("Installer: installArtifact", async (t) => {
 	);
 });
 
+test.serial("Installer: _fetchArtifactMetadata", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	sinon.stub(installer, "_synchronize").callsFake( async (pckg, callback) => await callback());
+	sinon.stub(installer, "_getLocalArtifactMetadata").resolves({lastCheck: 0, lastUpdate: 0});
+	sinon.stub(installer, "_getRemoteArtifactMetadata").resolves({revision: "1.22", lastUpdate: 0});
+	sinon.stub(installer, "_removeStaleRevisions").resolves();
+	sinon.stub(installer, "_writeLocalArtifactMetadata").resolves();
+
+	const artifactMetadata = await installer._fetchArtifactMetadata({
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		extension: "jar",
+	});
+
+	t.truthy(artifactMetadata.lastCheck, "Proper metadata: lastCheck");
+	t.is(artifactMetadata.lastUpdate, 0, "Proper metadata: lastUpdate");
+	t.is(artifactMetadata.revision, "1.22", "Proper metadata: revision");
+});
+
+test.serial("Installer: _fetchArtifactMetadata throws", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url",
+		cacheMode: "force"
+	});
+
+	sinon.stub(installer, "_synchronize").callsFake( async (pckg, callback) => await callback());
+	sinon.stub(installer, "_getLocalArtifactMetadata").resolves({});
+
+	await t.throwsAsync(installer._fetchArtifactMetadata({
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		extension: "jar",
+	}), {
+		message:
+			"Could not find artifact com.sap.ui5.dist:sapui5-sdk-dist:1.75.0:jar in local cache",
+	});
+});
+
+test.serial("Installer: _getRemoteArtifactMetadata", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	const registry = installer.getRegistry();
+	const requestMavenMetadataStub = sinon.stub(registry, "requestMavenMetadata")
+		.resolves({
+			versioning: {
+				snapshotVersions: {
+					snapshotVersion: [{"extension": "jar", "updated": "20220828080910", "value": "5.0.0-SNAPSHOT"}]
+				}
+			}
+		});
+
+	const remoteArtifactMetadata = await installer._getRemoteArtifactMetadata({
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		extension: "jar",
+	});
+
+	t.truthy(remoteArtifactMetadata.lastUpdate, "Proper metadata: lastUpdate");
+	t.is(remoteArtifactMetadata.revision, "5.0.0-SNAPSHOT", "Proper metadata: revision");
+
+	t.is(requestMavenMetadataStub.callCount, 1, "requestPackagePackument should be called once");
+	t.deepEqual(requestMavenMetadataStub.getCall(0).args[0],
+		{groupId: "com.sap.ui5.dist", artifactId: "sapui5-sdk-dist", version: "1.75.0"},
+		"requestMavenMetadata was called with correct arguments");
+});
+
+test.serial("Installer: _getRemoteArtifactMetadata throws", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	const registry = installer.getRegistry();
+	sinon.stub(registry, "requestMavenMetadata").resolves({});
+
+	await t.throwsAsync(installer._getRemoteArtifactMetadata({
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		extension: "jar",
+	}), {message: "Missing Maven snapshot metadata for artifact com.sap.ui5.dist:sapui5-sdk-dist:1.75.0"});
+});
+
+test.serial("Installer: _getRemoteArtifactMetadata throws missing deployment metadata", async (t) => {
+	const {Installer} = t.context;
+
+	const installer = new Installer({
+		cwd: "/cwd/",
+		ui5HomeDir: "/ui5Home/",
+		snapshotEndpointUrl: "endpoint-url"
+	});
+
+	const registry = installer.getRegistry();
+	sinon.stub(registry, "requestMavenMetadata")
+		.resolves({
+			versioning: {
+				snapshotVersions: {
+					snapshotVersion: [{"value": "5.0.0-SNAPSHOT"}]
+				}
+			}
+		});
+
+
+	await t.throwsAsync(installer._getRemoteArtifactMetadata({
+		groupId: "com.sap.ui5.dist",
+		artifactId: "sapui5-sdk-dist",
+		version: "1.75.0",
+		extension: "jar",
+	}), {message: "Could not find deployment undefined.jar for artifact " +
+	"com.sap.ui5.dist:sapui5-sdk-dist:1.75.0 in snapshot metadata:\n[{\"value\":\"5.0.0-SNAPSHOT\"}]"});
+});
