SAP/ui5-project

commit 1cd94f7f15ed07283e198238edb546517ee25691

[FIX] Project#getReader: Do not apply builder resource excludes for style 'runtime'
Introduce new style 'dist' to reflect the standard build output
structure which can be used with any HTTP server. It is identical to the
'runtime' style, but also applies any configured builder resource
excludes[1].

ProjectBuilder now uses style 'dist' instead of 'runtime'.

[1]: https://sap.github.io/ui5-tooling/stable/pages/Configuration/#exclude-resources

Fixes https://github.com/SAP/ui5-tooling/issues/779

lib/specifications/ComponentProject.js
@@ -92,29 +92,54 @@ class ComponentProject extends Project {
 	/* === Resource Access === */
 
 	/**
-	 * Get a resource reader for accessing the project resources in a given style.
+	 * Get a [ReaderCollection]{@link @ui5/fs/ReaderCollection} for accessing all resources of the
+	 * project in the specified "style":
+	 *
+	 * <ul>
+	 * <li><b>buildtime:</b> Resource paths are always prefixed with <code>/resources/</code>
+	 *  or <code>/test-resources/</code> followed by the project's namespace.
+	 *  Any configured build-excludes are applied</li>
+	 * <li><b>dist:</b> Resource paths always match with what the UI5 runtime expects.
+	 *  This means that paths generally depend on the project type. Applications for example use a "flat"-like
+	 *  structure, while libraries use a "buildtime"-like structure.
+	 *  Any configured build-excludes are applied</li>
+	 * <li><b>runtime:</b> Resource paths always match with what the UI5 runtime expects.
+	 *  This means that paths generally depend on the project type. Applications for example use a "flat"-like
+	 *  structure, while libraries use a "buildtime"-like structure.
+	 *  This style is typically used for serving resources directly. Therefore, build-excludes are not applied
+	 * <li><b>flat:</b> Resource paths are never prefixed and namespaces are omitted if possible. Note that
+	 *  project types like "theme-library", which can have multiple namespaces, can't omit them.
+	 *  Any configured build-excludes are applied</li>
+	 * </ul>
+	 *
 	 * If project resources have been changed through the means of a workspace, those changes
 	 * are reflected in the provided reader too.
 	 *
+	 * Resource readers always use POSIX-style paths.
+	 *
 	 * @public
 	 * @param {object} [options]
-	 * @param {string} [options.style=buildtime] Path style to access resources. Can be "buildtime", "runtime" or "flat"
-	 * 												TODO: describe styles
+	 * @param {string} [options.style=buildtime] Path style to access resources.
+	 *   Can be "buildtime", "dist", "runtime" or "flat"
 	 * @returns {@ui5/fs/ReaderCollection} A reader collection instance
 	 */
 	getReader({style = "buildtime"} = {}) {
 		// TODO: Additional style 'ABAP' using "sap.platform.abap".uri from manifest.json?
 
-		if (style === "runtime" && this._isRuntimeNamespaced) {
-			// If the project's runtime paths contains its namespace too,
-			// "runtime" style paths are identical to "buildtime" style paths
+		// Apply builder excludes to all styles but "runtime"
+		const excludes = style === "runtime" ? [] : this.getBuilderResourcesExcludes();
+
+		if ((style === "runtime" || style === "dist") && this._isRuntimeNamespaced) {
+			// If the project's type requires a namespace at runtime, the
+			// dist- and runtime-style paths are identical to buildtime-style paths
 			style = "buildtime";
 		}
-		let reader = this._getReader();
+		let reader = this._getReader(excludes);
 		switch (style) {
 		case "buildtime":
 			break;
 		case "runtime":
+		case "dist":
 			// Use buildtime reader and link it to /
 			// No test-resources for runtime resource access,
 			// unless runtime is namespaced
@@ -204,9 +229,9 @@ class ComponentProject extends Project {
 		return this._writers;
 	}
 
-	_getReader() {
-		let reader = this._getSourceReader();
-		const testReader = this._getTestReader();
+	_getReader(excludes) {
+		let reader = this._getSourceReader(excludes);
+		const testReader = this._getTestReader(excludes);
 		if (testReader) {
 			reader = resourceFactory.createReaderCollection({
 				name: `Reader collection for project ${this.getName()}`,
@@ -219,19 +244,20 @@ class ComponentProject extends Project {
 	_addWriter(reader, style) {
 		const {namespaceWriter, generalWriter} = this._getWriter();
 
-		if (style === "runtime" && this._isRuntimeNamespaced) {
-			// If the project's runtime requires namespaces, "runtime" paths are identical to "buildtime" paths
+		if ((style === "runtime" || style === "dist") && this._isRuntimeNamespaced) {
+			// If the project's type requires a namespace at runtime, the
+			// dist- and runtime-style paths are identical to buildtime-style paths
 			style = "buildtime";
 		}
 		const readers = [];
 		switch (style) {
-		case "buildtime": {
+		case "buildtime":
 			// Writer already uses buildtime style
 			readers.push(namespaceWriter);
 			readers.push(generalWriter);
 			break;
-		}
-		case "runtime": {
+		case "runtime":
+		case "dist":
 			// Runtime is not namespaced: link namespace to /
 			readers.push(resourceFactory.createFlatReader({
 				reader: namespaceWriter,
@@ -240,8 +266,7 @@ class ComponentProject extends Project {
 			// Add general writer as is
 			readers.push(generalWriter);
 			break;
-		}
-		case "flat": {
+		case "flat":
 			// Rewrite paths from "flat" to "buildtime"
 			readers.push(resourceFactory.createFlatReader({
 				reader: namespaceWriter,
@@ -249,7 +274,6 @@ class ComponentProject extends Project {
 			}));
 			// General writer resources can't be flattened, so they are not available
 			break;
-		}
 		default:
 			throw new Error(`Unknown path mapping style ${style}`);
 		}

commit 35ec7bec45331174a02f8123c65d4c2331233f7e

[FIX] ComponentProject#getWorkspace: Apply builder resource excludes
This resolves a regression caused by
https://github.com/SAP/ui5-project/pull/574 where the application of
builder resource-exclude configuration was moved from the low-level
_getSourceReader/_getTestReader methods to the high-level getReader API.

However, it was missed to also adapt the high-level getWorkspace API in
the same manner.

This caused the created workspace reader/writer to ignore any exclude
configuration as reported in
https://github.com/SAP/ui5-tooling/issues/784#issuecomment-1433625146

lib/specifications/ComponentProject.js
@@ -191,9 +191,11 @@ class ComponentProject extends Project {
 	 */
 	getWorkspace() {
 		// Workspace is always of style "buildtime"
+		// Therefore builder resource-excludes are always to be applied
+		const excludes = this.getBuilderResourcesExcludes();
 		return resourceFactory.createWorkspace({
 			name: `Workspace for project ${this.getName()}`,
-			reader: this._getReader(),
+			reader: this._getReader(excludes),
 			writer: this._getWriter().collection
 		});
 	}

test/lib/specifications/types/Application.js
@@ -155,6 +155,27 @@ test("Access project resources via reader w/ builder excludes", async (t) => {
 		"Found excluded resource for runtime style");
 });
 
+test("Access project resources via workspace w/ builder excludes", async (t) => {
+	const {projectInput} = t.context;
+	const baselineProject = await Specification.create(projectInput);
+
+	projectInput.configuration.builder = {
+		resources: {
+			excludes: ["**/manifest.json"]
+		}
+	};
+	const excludesProject = await Specification.create(projectInput);
+
+	// We now have two projects: One with excludes and one without
+	// Always compare the results of both to make sure a file is really excluded because of the
+	// configuration and not because of a typo or because of it's absence in the fixture
+
+	t.is((await baselineProject.getWorkspace().byGlob("**/manifest.json")).length, 1,
+		"Found resource in baseline project for default style");
+	t.is((await excludesProject.getWorkspace().byGlob("**/manifest.json")).length, 0,
+		"Did not find excluded resource for default style");
+});
+
 test("Modify project resources via workspace and access via flat and runtime readers", async (t) => {
 	const {projectInput} = t.context;
 	const project = await Specification.create(projectInput);

test/lib/specifications/types/Library.js
@@ -175,6 +175,27 @@ test("Access project resources via reader w/ builder excludes", async (t) => {
 		"Found excluded resource for runtime style");
 });
 
+test("Access project resources via workspace w/ builder excludes", async (t) => {
+	const {projectInput} = t.context;
+	const baselineProject = await (new Library().init(projectInput));
+
+	projectInput.configuration.builder = {
+		resources: {
+			excludes: ["**/.library"]
+		}
+	};
+	const excludesProject = await (new Library().init(projectInput));
+
+	// We now have two projects: One with excludes and one without
+	// Always compare the results of both to make sure a file is really excluded because of the
+	// configuration and not because of a typo or because of it's absence in the fixture
+
+	t.is((await baselineProject.getWorkspace().byGlob("**/.library")).length, 1,
+		"Found resource in baseline project for default style");
+	t.is((await excludesProject.getWorkspace().byGlob("**/.library")).length, 0,
+		"Did not find excluded resource for default style");
+});
+
 test("Modify project resources via workspace and access via flat and runtime reader", async (t) => {
 	const {projectInput} = t.context;
 	const project = await (new Library().init(projectInput));

test/lib/specifications/types/Module.js
@@ -144,6 +144,33 @@ test("Access project resources via reader w/ builder excludes", async (t) => {
 		"Found excluded resource for runtime style");
 });
 
+test("Access project resources via workspace w/ builder excludes", async (t) => {
+	const {projectInput, sinon} = t.context;
+	const baselineProject = await Specification.create(projectInput);
+	const excludesProject = await Specification.create(projectInput);
+
+	// As of specVersion 3.0, modules are not allowed to have a "builder.resources" configuration.
+	// Hence modules can't practically be configured with builder excludes.
+	// We still simply stub the respective API call to test the code and be prepared
+	//
+	// projectInput.configuration.builder = {
+	// 	resources: {
+	// 		excludes: ["**/devTools.js"]
+	// 	}
+	// };
+	// So stub instead:
+	sinon.stub(excludesProject, "getBuilderResourcesExcludes").returns(["**/devTools.js"]);
+
+	// We now have two projects: One with excludes and one without
+	// Always compare the results of both to make sure a file is really excluded because of the
+	// configuration and not because of a typo or because of it's absence in the fixture
+
+	t.is((await baselineProject.getWorkspace().byGlob("**/devTools.js")).length, 1,
+		"Found resource in baseline project for default style");
+	t.is((await excludesProject.getWorkspace().byGlob("**/devTools.js")).length, 0,
+		"Did not find excluded resource for default style");
+});
+
 test("Modify project resources via workspace and access via reader", async (t) => {
 	const {projectInput} = t.context;
 	const project = await Specification.create(projectInput);

test/lib/specifications/types/ThemeLibrary.js
@@ -115,6 +115,27 @@ test("Access project resources via reader w/ builder excludes", async (t) => {
 		"Found excluded resource for runtime style");
 });
 
+test("Access project resources via workspace w/ builder excludes", async (t) => {
+	const {projectInput} = t.context;
+	const baselineProject = await Specification.create(projectInput);
+
+	projectInput.configuration.builder = {
+		resources: {
+			excludes: ["**/.theme"]
+		}
+	};
+	const excludesProject = await Specification.create(projectInput);
+
+	// We now have two projects: One with excludes and one without
+	// Always compare the results of both to make sure a file is really excluded because of the
+	// configuration and not because of a typo or because of it's absence in the fixture
+
+	t.is((await baselineProject.getWorkspace().byGlob("**/.theme")).length, 1,
+		"Found resource in baseline project for default style");
+	t.is((await excludesProject.getWorkspace().byGlob("**/.theme")).length, 0,
+		"Did not find excluded resource for default style");
+});
+
 test("Modify project resources via workspace and access via flat and runtime reader", async (t) => {
 	const {projectInput} = t.context;
 	const project = await Specification.create(projectInput);