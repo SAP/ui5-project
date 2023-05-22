65496eabeaafc50348dfc276d19d135eb035b261
Merlin Beutlberger <m.beutlberger@sap.com>
2023-05-22 16:22:11 +0200

    [FIX] projectGraphBuilder: Add module cache invalidation (#612)
    If a visited module did not resolve to any specification (project or
    extensions), try recreating the module when visiting it again from a
    different parent.
    
    Resolves https://github.com/SAP/ui5-tooling/issues/807
    
    This is an alternative to https://github.com/SAP/ui5-project/pull/611
    
    This change might have a greater impact on performance since er might
    recreate modules that are not relevant to UI5 Tooling more than once if
    they are listed multiple times in the dependency tree.
    Before this change, such modules where only visited once.
    
    ---------
    
    Co-authored-by: Yavor Ivanov <yavor.ivanov@sap.com>

--- test/lib/graph/projectGraphBuilder.js
+++ test/lib/graph/projectGraphBuilder.js
@@ -674,6 +674,62 @@ test("Dependencies defined through shim", async (t) => {
 	t.deepEqual(graph.getDependencies("project-3"), ["project-2"], "Shimmed dependency has been defined");
 });
 
+test("Define external dependency as shims in sub-module", async (t) => {
+	t.context.getRootNode.resolves(createNode({
+		id: "app",
+		version: "1.0.0",
+		path: "/app"
+	}));
+
+	t.context.getDependencies.onCall(0).resolves([
+		createNode({
+			id: "lib",
+			version: "1.0.0",
+			path: "/lib"
+		}),
+		{
+			id: "external-thirdparty",
+			version: "1.0.0",
+			path: "/app/node_modules/external-thirdparty"
+		},
+		createNode({
+			id: "external-thirdparty-shim",
+			configuration: {
+				kind: "extension",
+				type: "project-shim",
+				shims: {
+					configurations: {
+						"external-thirdparty": {
+							specVersion: "3.0",
+							type: "module",
+							metadata: {name: "external-thirdparty"},
+							resources: {
+								configuration: {
+									paths: {"/resources/": ""},
+								},
+							},
+						},
+					},
+				},
+			}
+		})
+	]);
+
+	t.context.getDependencies.onCall(1).resolves([
+		createNode({
+			id: "external-thirdparty",
+			version: "1.0.0",
+			path: "/app/node_modules/external-thirdparty",
+			optional: false
+		})
+	]);
+
+	const graph = await projectGraphBuilder(t.context.provider);
+
+	t.deepEqual(graph.getDependencies("app"), ["lib"], "'app' depends on 'lib'");
+	t.deepEqual(graph.getDependencies("lib"), ["external-thirdparty"], "'lib' depends on 'external-thirdparty'");
+});
+
 test("Extension in dependencies", async (t) => {
 	t.context.getRootNode.resolves(createNode({
 		id: "id1",
