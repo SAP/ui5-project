9c50cad364cc981e39d9b0374f721bb39c827a06
Florian Vogt <florian.vogt@sap.com>
2024-01-16 15:24:29 +0100

    rename task

--- test/lib/build/definitions/library.js
+++ test/lib/build/definitions/library.js
@@ -99,7 +99,7 @@ test("Standard build", async (t) => {
 			}
 		},
 		generateLibraryManifest: {},
-		transformManifest: {},
+		enrichManifest: {},
 		generateLibraryPreload: {
 			options: {
 				excludes: [], skipBundles: []
@@ -236,7 +236,7 @@ test("Standard build with legacy spec version", (t) => {
 			}
 		},
 		generateLibraryManifest: {},
-		transformManifest: {},
+		enrichManifest: {},
 		generateLibraryPreload: {
 			options: {
 				excludes: [], skipBundles: []
@@ -358,7 +358,7 @@ test("Custom bundles", async (t) => {
 			}
 		},
 		generateLibraryManifest: {},
-		transformManifest: {},
+		enrichManifest: {},
 		generateLibraryPreload: {
 			options: {
 				excludes: [],
@@ -686,7 +686,7 @@ test("Standard build: nulled taskFunction to skip tasks", (t) => {
 			}
 		},
 		generateLibraryManifest: {},
-		transformManifest: {},
+		enrichManifest: {},
 		generateLibraryPreload: {
 			options: {
 				excludes: [], skipBundles: []
