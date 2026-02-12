9100a6dbdf48ce455df26fde856230b8fbc02553
Gianluca Beil <g.beil@sap.com>
2026-02-12 14:04:46 +0100

    [INTERNAL] Add comments to config

--- knip.config.js
+++ knip.config.js
@@ -1,6 +1,7 @@
 const config = {
 	/**
-	 * As we currently only need unused dependency checks, we disable all checks except for that
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
 	 */
 	rules: {
 		files: "off",
@@ -12,12 +13,25 @@ const config = {
 		catalog: "off",
 		exports: "off",
 		types: "off",
-		enumMembers: "off"
+		enumMembers: "off",
+		/**
+		 * We also ignore peer dependencies because @ui5/project
+		 * defines an optional peer dependency to @ui5/builder
+		 * which is needed and not an issue in our point of view.
+		 */
+		optionalPeerDependencies: "off"
 	},
 
 	ignoreDependencies: [
-		"@ui5/builder",
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
 		"@istanbuljs/esm-loader-hook",
+
+		/**
+		 * Used as jsdoc template in package.json script, which is not detected
+		 */
 		"docdash"
 	],
 };
